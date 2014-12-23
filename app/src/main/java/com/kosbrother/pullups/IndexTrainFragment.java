79c2a57b05fc914b0fec865744f52df2d91d0932
Steven Ko <chunyuko85@gmail.com>
2014-12-23 13:31:55 +0800

    training activity

--- app/src/main/java/com/kosbrother/pullups/IndexTrainFragment.java
+++ app/src/main/java/com/kosbrother/pullups/IndexTrainFragment.java
@@ -1,5 +1,6 @@
 package com.kosbrother.pullups;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.kosbrother.pullups.training_session.TraingingActivity;
 
 import java.util.Locale;
 
@@ -22,6 +25,7 @@ public class IndexTrainFragment extends Fragment {
 
     TextToSpeech ttobj;
     private Button startSessionButton;
+    private Button startRecordButton;
 
     public static IndexTrainFragment newInstance(String param1, String param2) {
         IndexTrainFragment fragment = new IndexTrainFragment();
@@ -66,13 +70,23 @@ public class IndexTrainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View fragment = inflater.inflate(R.layout.fragment_index_train, container, false);
-        startSessionButton = (Button)fragment.findViewById(R.id.startSession);
+        startSessionButton = (Button)fragment.findViewById(R.id.start_session_test);
         startSessionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 speakText();
             }
         });
+
+        startRecordButton = (Button)fragment.findViewById(R.id.start_record_test);
+        startRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent proIntent = new Intent(getActivity(), TraingingActivity.class);
+                startActivity(proIntent);
+            }
+        });
+
 
         return fragment;
     }
